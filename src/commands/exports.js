e0d94e0c85d9570dd3e2dcb2bb0dbdb40e68defe
terrablue <102580937+terrablue@users.noreply.github.com>
2023-05-23 22:47:08 +0300

    change lastseen to seen

--- src/commands/exports.js
+++ src/commands/exports.js
@@ -1,3 +1,3 @@
 export {default as band} from "./band.js";
 export {default as random} from "./random.js";
-export {default as lastseen} from "./lastseen.js";
+export {default as seen} from "./seen.js";
