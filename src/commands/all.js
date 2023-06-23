a17f7f9eb2f134b2761ab0cd77321bb70677d148
terrablue <102580937+terrablue@users.noreply.github.com>
2023-06-23 21:41:54 +0300

    account for lowercase names in db

--- src/commands/all.js
+++ src/commands/all.js
@@ -19,16 +19,18 @@ export default async (_, _2, {to, from, client}) => {
   const users = await new Path(import.meta.url).up(2).join("db", "users.json")
     .json();
   const present = await getNames(client, to);
+  const presentlc = present.map(p => p.toLowerCase());
   const senders = [bot, from];
   const names = transform(users, entry => entry
     // exclude users not in channel and the bot
-    .filter(([name]) => present.includes(name) && !senders.includes(name))
+    .filter(([name]) => presentlc.includes(name) && !senders.includes(name))
     // exclude users who have never spoken
     .filter(([, data]) => data.message !== undefined)
-    .map(([user, data]) => [user, new Date(data.message)])
-
-    .toSorted(([, a], [, b]) => b.getTime() > a.getTime() ? 1 : -1)
+    // map key to names, value to dates
+    .map(([user, data]) =>
+      [presentlc.find(p => p === user), new Date(data.message).getTime()])
+    .toSorted(([, a], [, b]) => Math.sign(b > a))
     .filter((_, i) => i < limit));
 
-  return [`^ ${Object.keys(names).join(", ")}`];
+  return [`${Object.keys(names).join(", ")}`];
 };
