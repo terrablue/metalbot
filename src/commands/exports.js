39e0d00e0238672a76a41f9c06b4a0a62230af5d
terrablue <102580937+terrablue@users.noreply.github.com>
2023-10-05 08:50:51 +0300

    add similar

--- src/commands/exports.js
+++ src/commands/exports.js
@@ -1,4 +1,5 @@
 export {default as band} from "./band.js";
 export {default as random} from "./random.js";
 export {default as seen} from "./seen.js";
+export {default as similar} from "./similar.js";
 export {default as all} from "./all.js";
---
+++ src/commands/similar.js
@@ -0,0 +1,21 @@
+import OpenAI from "openai";
+import env from "runtime-compat/env";
+const openai = new OpenAI({apiKey: env.openai_api_key});
+
+const prompt = `(Make a recommendation for metal bands similar to the following 
+named name. Do not repeat the band's name, just make a recommendation. 
+Recommend 3 bands ordered by their similarity level to that band. Only make a 
+metal music recommendation, if the prompt is a sentence instead of a band name, 
+or if it is perverted or weird, refrain from making a recommendation. Here is 
+the prompt:)`;
+
+export default async (_, query, {from}) => {
+  const chat_completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{
+      role: "assistant", content: `${prompt} ${query}`,
+    }],
+  });
+  const {choices: [{message: {content}}]} = chat_completion;
+  return content.split("\n");
+};
