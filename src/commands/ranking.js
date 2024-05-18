30364ef51c426748b67a2cd7614283b0c522db08
terrablue <102580937+terrablue@users.noreply.github.com>
2024-05-18 13:25:34 +0200

    add invisible character to avoid highlight

--- src/commands/ranking.js
+++ src/commands/ranking.js
@@ -3,6 +3,7 @@ import { db } from "../karma.js";
 const cutoff = 2;
 
 const ranking = async () => Object.entries(Object.entries(await db.read())
+  .map(([key, value]) => [`${key.slice(0, -1)}\u200d${key.at(-1)}`, value])
   .reduce((folded, [key, value]) =>
     ({ ...folded, [value]: folded[value] ? [...folded[value], key] : [key] })
   , {}))
